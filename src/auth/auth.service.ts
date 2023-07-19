d270a9678ca1e3bd94ad82ac5e0738c9c536861e
cesar castillo <cesarcastillob23@gmail.com>
2023-07-19 13:07:14 -0500

    modificacion del service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,16 @@
 import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './entities/user.entity';
-import { Model } from 'mongoose';
-import * as bcryptjs from 'bcryptjs'
 import { JwtService } from '@nestjs/jwt';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs';
+
+import { RegisterUserDto, CreateUserDto, UpdateAuthDto, LoginDto } from './dto';
+
+import { User } from './entities/user.entity';
+
 import { JwtPayload } from './interfaces/jwt-payload';
 import { LoginResponse } from './interfaces/login-response';
-import { RegisterUser } from './interfaces/register.user';
-
-import { CreateUserDto,RegisterUserDto,LoginDto,UpdateAuthDto } from './dto';
-
-
 
 @Injectable()
 export class AuthService {
@@ -22,68 +22,77 @@ export class AuthService {
     private jwtService: JwtService,
    ) {}
 
-
-  async create(createUserDto: CreateUserDto):Promise<User> {
+  
+  async create(createUserDto: CreateUserDto): Promise<User> {
     
-    try{
-
-      const {password,...userData}=createUserDto
-      const newUser= new this.userModel({
-        password:bcryptjs.hashSync(password,10),
-        ...userData
-      })
-      await newUser.save();
-      const {password:_,...user}= newUser.toJSON()
-
-      return user
-
-    }catch(error){
-      if(error.code === 11000){
-        throw new BadRequestException(`${createUserDto.email} alredy exists!`)
-      }
-        throw new InternalServerErrorException('Something terrible happen!!!')
-      }
-    
-  }
-
-  async register(registerDto:RegisterUserDto):Promise<RegisterUser>{
-
-    const user = await this.create(registerDto)
-
-
-     return {
-      user,
-      token:this.getJwtToken({id:user._id})
-    }
-  }
-
-  async login(loginDto:LoginDto):Promise<LoginResponse>{
-    const{email,password}=loginDto;
-    const user =await this.userModel.findOne({email})
-    if(!user){
-      throw new UnauthorizedException('Not valid credentials - email')
-    }
-    if(!bcryptjs.compareSync(password,user.password)){
+    try {
       
-      throw new UnauthorizedException('Not valid credentials - password')
+      const { password, ...userData } = createUserDto;
+           
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync( password, 10 ),
+        ...userData
+      });
+
+       await newUser.save();
+       const { password:_, ...user } = newUser.toJSON();
+       
+       return user;
+      
+    } catch (error) {
+      if( error.code === 11000 ) {
+        throw new BadRequestException(`${ createUserDto.email } already exists!`)
+      }
+      throw new InternalServerErrorException('Something terribe happen!!!');
     }
 
-    const{password:_,...rest}=user.toJSON()
+  }
+
+  async register( registerDto: RegisterUserDto ): Promise<LoginResponse> {
+
+    const user = await this.create( registerDto );
+
     return {
-      user:rest,
-      token:this.getJwtToken({id:user.id}),
+      user: user,
+      token: this.getJwtToken({ id: user._id })
     }
   }
 
-  findAll():Promise<User[]> {
-    return this.userModel.find()
+
+  async login( loginDto: LoginDto ):Promise<LoginResponse> {
+
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email });
+    if ( !user ) {
+      throw new UnauthorizedException('Not valid credentials - email');
+    }
+    
+    if ( !bcryptjs.compareSync( password, user.password ) ) {
+      throw new UnauthorizedException('Not valid credentials - password');
+    }
+
+    const { password:_, ...rest  } = user.toJSON();
+
+      
+    return {
+      user: rest,
+      token: this.getJwtToken({ id: user.id }),
+    }
+  
   }
 
-  async findUserById(id:string){
-    const user = await this.userModel.findById(id);
-    const {password,...rest}=user.toJSON()
-    return rest
+
+  findAll(): Promise<User[]> {
+    return this.userModel.find();
   }
+
+  async findUserById( id: string ) {
+    const user = await this.userModel.findById( id );
+    const { password, ...rest } = user.toJSON();
+    return rest;
+  }
+
 
   findOne(id: number) {
     return `This action returns a #${id} auth`;
@@ -97,12 +106,9 @@ export class AuthService {
     return `This action removes a #${id} auth`;
   }
 
-  getJwtToken(payload:JwtPayload){
-    const token =this.jwtService.sign(payload);
+  getJwtToken( payload: JwtPayload ) {
+    const token = this.jwtService.sign(payload);
     return token;
-
   }
 
-
 }
-
