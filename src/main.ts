65ccea87a175f4df4a7d418c7ef5c1d3b87fa0ec
cesar castillo <cesarcastillob23@gmail.com>
2023-07-19 12:01:39 -0500

    modificacion del port

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { log } from 'console';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,10 @@ async function bootstrap() {
       forbidNonWhitelisted:true,
     })
   )
-  await app.listen(process.env.PORT ?? 3000);
+
+  const PORT=process.env.PORT ?? 3000
+  console.log(`App corriendo en puerto :${PORT}`);
+  
+  await app.listen(PORT);
 }
 bootstrap();
